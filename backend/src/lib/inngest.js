32820d121816bf2bc3070206e56cf72fef215033
Akshatt1295 <akshatanwar1295@gmail.com>
2026-04-01 23:45:23 +0530

    stream changes

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
 import { connectDB } from "./db.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "coding-platform" });
 
@@ -20,6 +21,12 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
+
+    await upsertStreamUser({
+        id: newUser.clerkId.toString(),
+        name: newUser.name,
+        image: newUser.profileImage
+    });
   },
 );
 
@@ -31,6 +38,7 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
 
     await User.deleteOne({ clerkId: id });
+    await deleteStreamUser(id.toString());
   },
 );
 
