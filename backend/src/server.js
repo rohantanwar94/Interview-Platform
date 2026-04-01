eb17fec09002b72a2bf5392eff03dc686f7131b0
Akshatt1295 <akshatanwar1295@gmail.com>
2026-04-01 22:51:17 +0530

    auth-db-sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import path from "path";
-
+import {serve} from 'inngest';
 import {ENV} from './lib/env.js';
 import { connectDB } from './lib/db.js';
+import cors from 'cors';
+import { functions, inngest } from './lib/inngest.js';
 const app = express();
 const __dirname = path.resolve();
 
@@ -17,6 +19,11 @@ if(ENV.NODE_ENV === "production"){
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
+
+//middleware
+app.use(express.json());
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
+app.use("/api/inngest", serve({ client: inngest, functions}))
 
 const startServer = async() =>{
     try {
