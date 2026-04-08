1f88e7c4f5832bb45e5127000db32ee48724d107
Akshatt1295 <akshatanwar1295@gmail.com>
2026-04-08 15:45:55 +0530

    added dashboard and session

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -6,16 +6,17 @@ export const protectRoute = [
   async (res, req, next) => {
     try {
       const clerkId = res.auth().userId;
+      console.log('rohanss', clerkId);
       if (!clerkId)
         return res.status(401).json({ msg: "Unauthorized - invalid token" });
 
       //find user in db
       const user = await User.findOne({ clerkId });
       if (!user) return res.status(401).json({ msg: "user not found" });
-
+      console.log('rohan uswer', user);
       // attach auth to user
       req.user = user;
-
+     console.log('rohan req', req.user);
       next();
     } catch (error) {
       console.error("Error in protect Route: ", error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,44 +1,49 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-import cors from "cors";
-import { functions, inngest } from "./lib/inngest.js";
-import { clerkMiddleware } from '@clerk/express'
+import { inngest, functions } from "./lib/inngest.js";
+
 import chatRoutes from "./routes/chatRoutes.js";
-import sessionRoutes from './routes/sessionRoutes.js';
+import sessionRoutes from "./routes/sessionRoutes.js";
+
 const app = express();
+
 const __dirname = path.resolve();
 
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
+
 app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "success form api 345353" });
+  res.status(200).json({ msg: "api is up and running" });
 });
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/{*any}", (res, req) => {
+
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-//middleware
-app.use(express.json());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use('/api/chat', chatRoutes);
-app.use('/api/sessions', sessionRoutes);
-app.use(clerkMiddleware()); // this adds auth field to request object: req.auth() 
-
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () =>
-      console.log("Server is running on port", ENV.PORT),
-    );
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
-    console.error(error);
+    console.error("💥 Error starting the server", error);
   }
 };
 
